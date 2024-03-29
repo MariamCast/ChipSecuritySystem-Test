7dd1b22e6b8d1a09d878c33186c8acce0ad4909f
MariamCast <mariamcastaneda.rmz@gmail.com>
2024-03-29 12:18:42 -0600

    - Added new class call it Chips inside the project with 2 new methods :
    - VerifyValidChips
    - SortChips

---
+++ Chips.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChipSecuritySystem.Chips
+{
+    public static class Chips
+    {
+        public static List<Tuple<Color, Color>> SortChips(List<Tuple<Color, Color>> chips)
+        {
+            try
+            {
+                var color = Color.Blue;
+                for (int i = 0; i < chips.Count; i++)
+                {
+                    if (chips[i].Item1 != color)
+                    {
+                        var temp = chips[i];
+                        chips[i] = chips[i + 1];
+                        chips[i + 1] = temp;
+
+                    }
+                    color = chips[i].Item2;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something wen wrong when sorting the chips {ex.Message}");
+            }
+            return chips;
+        }
+
+        public static List<Tuple<Color, Color>> VerifyValidChips(List<Tuple<Color, Color>> chips)
+        {
+            List<Tuple<Color, Color>> chipsToRemove = new List<Tuple<Color, Color>>();
+            try
+            {
+                bool validChipColor1 = true;
+                bool validChipColor2 = true;
+                foreach (Tuple<Color, Color> chip in chips)
+                {
+                    if (chip.Item1 != Color.Blue)
+                    {
+                        bool evenColorItem1 = EvenColorCompareWithItem2(chip.Item1, chips);
+
+                        validChipColor1 = evenColorItem1;
+                    }
+
+                    if (chip.Item2 != Color.Green)
+                    {
+                        bool evenColorItem2 = EvenColorCompareWithItem1(chip.Item2, chips);
+                        validChipColor2 = evenColorItem2;
+                    }
+
+                    if (!validChipColor1 || !validChipColor2)
+                        chipsToRemove.Add(chip);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Something wen wrong when verifying is the chips are valid {ex.Message}");
+            }
+
+            List<Tuple<Color, Color>> newChipList = chips.Except(chipsToRemove).ToList();
+            return newChipList;
+        }
+        private static bool EvenColorCompareWithItem1(Color color, List<Tuple<Color, Color>> chips)
+        {
+            var count = (chips.Where(c => color == c.Item1).Count() + 1) % 2;
+            return count == 0 ? true : false;
+        }
+        private static bool EvenColorCompareWithItem2(Color color, List<Tuple<Color, Color>> chips)
+        {
+            var count = (chips.Where(c => color == c.Item2).Count() + 1) % 2;
+            return count == 0 ? true : false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChipSecuritySystem
 {
@@ -10,6 +8,75 @@ namespace ChipSecuritySystem
     {
         static void Main(string[] args)
         {
+            //Tuple<Color, Color> chip1 = Tuple.Create(Color.Blue, Color.Yellow);
+            //Tuple<Color, Color> chip2 = Tuple.Create(Color.Red, Color.Green);
+            //Tuple<Color, Color> chip3 = Tuple.Create(Color.Yellow, Color.Red);
+            //Tuple<Color, Color> chip4 = Tuple.Create(Color.Orange, Color.Purple);
+
+            try
+            {
+                Console.WriteLine(@"Please Enter how many chips you have: ");
+                string chipsCount = Console.ReadLine();
+                int numberOfChips = Convert.ToInt32(chipsCount);
+                List<Tuple<Color, Color>> chips = new List<Tuple<Color, Color>>();
+                for (int i = 0; i < numberOfChips; i++)
+                {
+                    Console.WriteLine($"Colors available:  Red,Green,Blue,Yellow,Purple,Orange");
+
+                    Console.WriteLine($"Please enter Color 1 for chip {i + 1}: ");
+                    string item1 = Console.ReadLine();
+                    Console.WriteLine($"Please enter Color 2 for chip {i + 1}: ");
+                    string item2 = Console.ReadLine();
+
+                    try
+                    {
+                        Color color1 = (Color)Enum.Parse(typeof(Color), item1);
+                        Color color2 = (Color)Enum.Parse(typeof(Color), item2);
+                        Tuple<Color, Color> chip1 = Tuple.Create(color1, color2);
+                        chips.Add(chip1);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"The color you provided is wrong {ex.Message}");
+                    }
+                }
+                var foundTupleBlue = chips.Where(c => c.Item1 == Color.Blue);
+                if (!foundTupleBlue.Any())
+                {
+                    throw new KeyNotFoundException("A chip with blue color was not found");
+                }
+
+                var foundTupleGreen = chips.Where(c => c.Item2 == Color.Green);
+                if (!foundTupleGreen.Any())
+                {
+                    throw new KeyNotFoundException("A chip with green color was not found");
+                }
+
+                var newChipList = Chips.Chips.VerifyValidChips(chips);
+                if (newChipList.Count() <= 0)
+                {
+                    throw new Exception($"{Constants.ErrorMessage}. Please verify the chips has colors to match");
+                }
+
+                var sortedChipList = Chips.Chips.SortChips(newChipList);
+                Console.WriteLine($"\nResult:");
+
+                foreach (var chip in sortedChipList)
+                {
+                    ColorChip colorChip = new ColorChip(chip.Item1, chip.Item2);
+
+                    Console.WriteLine($"{colorChip.ToString()}\n");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"An error ocurred: {ex.Message}");
+            }
+            Console.ReadLine();
         }
+
     }
 }
+
+
